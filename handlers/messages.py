ebe5d6a2bfe1becebdc88c7d4c95d156ead6c41d
Daniel G. Taylor <dan@programmer-art.org>
2012-08-13 08:32:04 -0700

    Implement basic message sending and marking of messages as read when a user views them.

--- handlers/messages.py
+++ handlers/messages.py
@@ -1,8 +1,10 @@
+import cgi
+import json
 import webapp2
 
 from models.message import Message
 from models.userprefs import UserPrefs
-from util import render
+from util import render, render_json
 from webapp2_extras.appengine.users import login_required
 
 
@@ -20,3 +22,127 @@ class MessagesHandler(webapp2.RequestHandler):
         to the currently logged in user.
         """
         render(self, 'messages.html')
+
+    def post(self):
+        """
+        Send a new message to a user.
+        """
+        user = UserPrefs.get()
+
+        if not user:
+            return render_json(self, {
+                'status': 'error',
+                'error': 'User not logged in'
+            })
+
+        recipient_name = cgi.escape(self.request.get('recipient'))
+        body = cgi.escape(self.request.get('body'))
+
+        recipient = UserPrefs.all()\
+                             .filter('name =', recipient_name)\
+                             .get()
+
+        if not recipient:
+            return render_json(self, {
+                'status': 'error',
+                'error': 'Recipient %(recipient_name)s not found' % locals()
+            })
+
+        msg = Message()
+        msg.user_from = user
+        msg.user_to = recipient
+        msg.body = body
+        msg.put()
+
+        # TODO: put this into a transaction
+        recipient.unread_messages += 1
+        recipient.put()
+
+        render_json(self, {
+            'status': 'ok'
+        })
+
+    def put(self):
+        """
+        Update a message as read or unread.
+        """
+        message_json = cgi.escape(self.request.get('message'))
+
+        # Parse the JSON into Python objects
+        try:
+            message_data = json.loads(message_json)
+        except Exception, e:
+            render_json(self, {
+                'status': 'error',
+                'error': str(e),
+                'input': message_json
+            })
+            return
+
+        msg = Message.get_by_id(message_data['id'])
+
+        if not msg:
+            return render_json(self, {
+                'status': 'error',
+                'error': 'Message %(id)s not found' % message_data
+            })
+
+        if msg.read == message_data['read']:
+            return render_json(self, {
+                'status': 'error',
+                'error': 'Message already marked, no update needed'
+            })
+
+        # Update message read property
+        msg.read = message_data['read']
+        msg.put()
+
+        # Update user unread message cache
+        recipient = msg.user_to
+        if msg.read:
+            recipient.unread_messages -= 1
+        else:
+            recipient.unread_messages += 1
+        recipient.put()
+
+        render_json(self, {
+            'status': 'ok'
+        })
+
+    def delete(self):
+        """
+        Delete a message.
+        """
+        message_json = cgi.escape(self.request.get('message'))
+
+        # Parse the JSON into Python objects
+        try:
+            message_data = json.loads(message_json)
+        except Exception, e:
+            render_json(self, {
+                'status': 'error',
+                'error': str(e),
+                'input': message_json
+            })
+            return
+
+        msg = Message.get_by_id(message_data['id'])
+
+        if not msg:
+            return render_json(self, {
+                'status': 'error',
+                'error': 'Message %(id)s not found' % message_data
+            })
+
+        # Update unread message cache if needed
+        if not msg.read:
+            recipient = msg.user_to
+            recipient.unread_messages -= 1
+            recipient.put()
+
+        # Delete this message from the data store
+        msg.delete()
+
+        render_json(self, {
+            'status': 'ok'
+        })
--- static/scripts/messages.coffee
+++ static/scripts/messages.coffee
@@ -2,14 +2,69 @@ class Messages
     # Initialize the messages class by attaching to message click events
     # to setup message rendering and making the latest message selected
     @init: =>
-        $('.messageList').on('click', 'tr', (event) =>
-            # Update the live message preview by parsing the message body
-            # with marked, a Markdown implementation for javascript
-            $('#messagePreview').html(marked(event.currentTarget.dataset.body))
+        $('#sendMessage').click(@send)
 
-            $('.messageList td').removeClass('active')
-            $(event.currentTarget).children().addClass('active')
-        )
+        $('.messageList').on('click', 'tr', @selected)
 
         # Set first received message as active and show preview
         $('.messageList tr:first').slice(0, 1).click()
+
+    # Handle user clicks on the message list
+    @selected: (event) =>
+        row = $(event.currentTarget)
+        # Update the live message preview by parsing the message body
+        # with marked, a Markdown implementation for javascript
+        $('#messagePreview').html(marked(event.currentTarget.dataset.body))
+
+        # Set this message as active
+        $('.messageList td').removeClass('active')
+        row.children().addClass('active')
+
+        # Mark as read if needed
+        if row.hasClass('unread')
+            @mark(parseInt(row.attr('id').substr(7)), true)
+
+    # Send a new message to a user
+    @send: =>
+        # Send this message via an AJAX call, then let the user know
+        # whether it was properly sent or if there was an error.
+        $.ajax(
+            url: '/messages'
+            type: 'post'
+            data:
+                recipient: $('#messageRecipient').val()
+                body: $('#messageBody').val()
+            success: (data, status, xhr) =>
+                # TODO: handle errors
+        )
+
+    # Mark a message as read or unread
+    @mark: (id, read) =>
+        $('#message' + id).removeClass('unread')
+
+        $.ajax(
+            url: '/messages'
+            type: 'put'
+            data:
+                message: JSON.stringify(
+                    id: id
+                    read: read
+                )
+            success: (data, status, xhr) =>
+                # TODO: handle errors
+        )
+
+    # Delete a message
+    @delete: (id) =>
+        $('#message' + id).remove()
+
+        $.ajax(
+            url: '/messages'
+            type: 'delete'
+            data:
+                message: JSON.stringify(
+                    id: id
+                )
+            success: (data, status, xhr) =>
+                # TODO: handle errors
+        )
