51c76493f6f2234de52bbea02906b5224aa9d182
Daniel G. Taylor <dan@programmer-art.org>
2012-10-29 18:24:56 -0700

    Update docs generation script to properly generate all coffeescript docs

--- genDocs.sh
+++ genDocs.sh
@@ -3,4 +3,4 @@
 pycco *.py
 pycco -d docs/handlers handlers/*.py
 pycco -d docs/models models/*.py
-pycco -d docs/static static/scripts/*.coffee
+pycco -d docs/static static/scripts/*.coffee static/scripts/main/*.coffee
